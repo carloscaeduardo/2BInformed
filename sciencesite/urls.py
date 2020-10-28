612e74c47f134522f9d53a8aa9f9d18ffbf7ffe8
Carlos <carlos.ca.eduardo@gmail.com>
2020-10-27 22:07:10 -0300

    update in base.html resources

--- sciencesite/urls.py
+++ sciencesite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('accounts/login/', views.login, name='login'),
+    path('account/logout/', views.logout, name='logout', kwargs={'next_page'})
 ]
