b71678aa0060dbcabb85a9803123211a2ac85a11
carloscaeduardo <carlos.ca.eduardo@gmail.com>
2021-01-10 16:33:36 +0000

    Jan 10 2021 added some css to base.html

--- blog/models.py
+++ blog/models.py
@@ -11,27 +11,27 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length = 200)
     text = RichTextUploadingField()
-    snippet_image = models.ImageField(blank = True, null = True)
+    snippet_image = models.ImageField(blank = True, null = True, help_text={"use images in 3x2 proportion!" })
     created_date = models.DateTimeField(default = timezone.now)
     publication_date = models.DateTimeField(blank = True, null=True)
     slug = models.SlugField(allow_unicode=True, unique = True)
     subject = models.CharField(max_length= 200, blank = True, null = True)
     def publish(self):
         self.publication_date = timezone.now()
-        
+
         self.save()
-     
-       
- 
+
+
+
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-        
+
     def get_absolute_url(self):
         return reverse("post_detail", kwargs = {'slug':self.slug })
-    
-     
+
+
 
