1550a2af972f43b4b9e9ff25fcca044211cb196b
carloscaeduardo <carlos.ca.eduardo@gmail.com>
2021-02-22 22:15:06 +0000

    adding css styles to post-list up to medium

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,7 +1,22 @@
-$('.post-slick').slick({
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+if (window.matchMedia("(max-width: 992px)").matches) {
+  /* The viewport is less than, or equal to, 992 pixels wide */
+      $('.post-slick').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        });
+} else {
+    $('.post-slick').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+        });
+  /* The viewport is greater than 700 pixels wide */
+}
+
+
+
